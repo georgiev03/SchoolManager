79b33212d77653d0c769568f1f5c20118e3ad474
Georgi Georgiev <reno199010@gmail.com>
2022-03-19 13:10:24 +0200

    Add project files.

---
+++ SchoolManager.Core/Constants/FormattingConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace SchoolManager.Core.Constants
+{
+    public class FormattingConstant
+    {
+        public const string NormalDateFormat = "dd.MM.yyyy";
+    }
+}
---
+++ SchoolManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

c41dfb1b8d0beca5406f025324da7b9a47002fb5
Georgi Georgiev <reno199010@gmail.com>
2022-03-20 16:51:33 +0200

    Changed login model

--- SchoolManager/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SchoolManager/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -70,6 +70,13 @@ namespace SchoolManager.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            //[Required]
+            //[StringLength(15)]
+            //public string FirstName { get; set; }
+
+            //[Required]
+            //[StringLength(15)]
+            //public string LastName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
